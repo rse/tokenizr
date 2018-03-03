d1d5a23f889e0d8edfdd464b6ce564ce69989d3c
Ralf S. Engelschall <rse@engelschall.com>
2018-03-03 10:48:18 +0100

    switch source code to ES6 fully

--- src/tokenizr.js
+++ src/tokenizr.js
@@ -24,25 +24,23 @@
 
 /*  utility function: create a source excerpt  */
 const excerpt = (txt, o) => {
-    var l = txt.length;
-    var b = o - 20; if (b < 0) b = 0;
-    var e = o + 20; if (e > l) e = l;
-    var hex = function (ch) {
-        return ch.charCodeAt(0).toString(16).toUpperCase();
-    };
-    var extract = function (txt, pos, len) {
-        return txt.substr(pos, len)
+    let l = txt.length
+    let b = o - 20; if (b < 0) b = 0
+    let e = o + 20; if (e > l) e = l
+    let hex = (ch) =>
+        ch.charCodeAt(0).toString(16).toUpperCase()
+    const extract = (txt, pos, len) =>
+        txt.substr(pos, len)
             .replace(/\\/g,   "\\\\")
             .replace(/\x08/g, "\\b")
             .replace(/\t/g,   "\\t")
             .replace(/\n/g,   "\\n")
             .replace(/\f/g,   "\\f")
             .replace(/\r/g,   "\\r")
-            .replace(/[\x00-\x07\x0B\x0E\x0F]/g, function(ch) { return "\\x0" + hex(ch); })
-            .replace(/[\x10-\x1F\x80-\xFF]/g,    function(ch) { return "\\x"  + hex(ch); })
-            .replace(/[\u0100-\u0FFF]/g,         function(ch) { return "\\u0" + hex(ch); })
-            .replace(/[\u1000-\uFFFF]/g,         function(ch) { return "\\u"  + hex(ch); });
-    }
+            .replace(/[\x00-\x07\x0B\x0E\x0F]/g, (ch) => "\\x0" + hex(ch))
+            .replace(/[\x10-\x1F\x80-\xFF]/g,    (ch) => "\\x"  + hex(ch))
+            .replace(/[\u0100-\u0FFF]/g,         (ch) => "\\u0" + hex(ch))
+            .replace(/[\u1000-\uFFFF]/g,         (ch) => "\\u"  + hex(ch))
     return {
         prologTrunc: b > 0,
         prologText:  extract(txt, b, o - b),
